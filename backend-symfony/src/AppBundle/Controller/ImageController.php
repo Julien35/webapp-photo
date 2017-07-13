d23f9d9d778692506d1c98d702fcf208a1cf8665
BERTHAU Julien <julien.berthau@orange.com>
2017-07-13 17:13:36 +0200

    Add fisrt part for vich bundle

--- backend-symfony/src/AppBundle/Controller/ImageController.php
+++ backend-symfony/src/AppBundle/Controller/ImageController.php
@@ -2,18 +2,21 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\ImageProduct;
+use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 
 class ImageController extends Controller
 {
     /**
-     * @Route("/image/upload", name="image_by_slug")
+     * @Route("/image/upload", name="image-by-slug")
      * @Method({"POST"})
      *
      * @param Request $request
@@ -22,21 +25,42 @@ class ImageController extends Controller
      */
     public function uploadAction(Request $request)
     {
-        // retrieve the file with the name given in the form.
-        // do var_dump($request->files->all()); if you need to know if the file is being uploaded.
         $file = $request->files->get('file');
         $status = array('status' => "success", "fileUploaded" => false);
 
-        // If a file was uploaded
-        if (!is_null($file)) {
+        if ($file instanceof UploadedFile && !is_null($file)) {
             // generate a random name for the file but keep the extension
-            $filename = uniqid() . "." . $file->getClientOriginalExtension();
-            $path = "images/products";
-            $file->move($path, $filename); // move the file to a path
-            $status = array('status' => "success", "fileUploaded" => true);
+//            $filename = uniqid() . "." . $file->getClientOriginalExtension();
+//            $path = "images/products";
+//            $file->move($path, $filename); // move the file to a path
+//            $status = array('status' => "success", "fileUploaded" => true);
+
+            // use of vich bundle
+            $product = new Product();
+
+            $formFactory = Forms::createFormFactoryBuilder()
+                ->addExtension(new HttpFoundationExtension())
+                ->getFormFactory();
+
+            $form = $formFactory->createBuilder()
+                ->add('title', 'text')
+                ->add('imageFile', 'file')
+                ->add('save', 'submit')
+                ->getForm();
+
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                // On l'enregistre notre objet $advert dans la base de données, par exemple
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($product);
+                $em->flush();
+            }
+
         }
 
         return new JsonResponse($status);
     }
 
 }
+
