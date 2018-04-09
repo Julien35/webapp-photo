82c41b844e78aa3658cbbed8cc3177b685984abb
AD-SUBS\bvxg8510 <julien.berthau@orange.com>
2018-04-09 11:52:54 +0200

    Front : cart, add address

--- assets/vuejs/components/ShoppingCart.vue
+++ assets/vuejs/components/ShoppingCart.vue
@@ -1,60 +1,81 @@
 <template>
 
-    <section class="">
-        <table id="cart" class="table table-hover table-condensed">
+    <section>
+        <article>
+            <h3>Résumée de la commande</h3>
+            <table id="cart" class="table table-hover table-condensed">
 
-            <thead>
-            <tr>
-                <th>Produits</th>
-                <th>Prix</th>
-                <th>Quantités</th>
-                <th class="text-center">SousTotal</th>
-                <th></th>
-            </tr>
-            </thead>
+                <thead>
+                <tr>
+                    <th>Produits</th>
+                    <th>Prix</th>
+                    <th>Quantités</th>
+                    <th class="text-center">SousTotal</th>
+                    <th></th>
+                </tr>
+                </thead>
 
-            <tbody v-if="files.length > 0">
-            <tr v-for="(file, key) in files" @change="getImagePreviews()">
-                <td>
-                    <div class="row">
-                        <div class="col-4 thumbnail">
-                            <img src="" class="preview img-thumbnail" v-bind:ref="'preview'+parseInt( key )"/>
+                <tbody v-if="files.length > 0">
+                <tr v-for="(file, key) in files" @change="getImagePreviews()">
+                    <td>
+                        <div class="row">
+                            <div class="col-4 thumbnail">
+                                <img src="" class="preview img-thumbnail" v-bind:ref="'preview'+parseInt( key )"/>
+                            </div>
+                            <div class="col-8">
+                                <div>{{file.nameText}}</div>
+                                <div>{{file.format}}</div>
+                                <div>{{file.finition}}</div>
+                            </div>
                         </div>
-                        <div class="col-8">
-                            <div>{{file.nameText}}</div>
-                            <div>{{file.format}}</div>
-                            <div>{{file.finition}}</div>
-                        </div>
-                    </div>
-                </td>
+                    </td>
 
-                <td>1.99€</td>
+                    <td>1.99€</td>
 
-                <td>
-                    <label>
-                        <input type="number" min="1" class="form-control text-center" v-on:click="updateCart"
-                               v-model:value="file.quantity">
-                    </label>
-                </td>
+                    <td>
+                        <label>
+                            <input type="number" min="1" class="form-control text-center" v-on:click="updateCart"
+                                   v-model:value="file.quantity">
+                        </label>
+                    </td>
 
-                <td class="text-center">{{ file.quantity * 1.99}}</td>
+                    <td class="text-center">{{ file.quantity * 1.99}}</td>
 
-                <td>
-                    <a class="btn btn-danger" v-on:click="removeFile( key )">Supprimer</a>
-                </td>
-            </tr>
-            </tbody>
+                    <td>
+                        <a class="btn btn-danger" v-on:click="removeFile( key )">Supprimer</a>
+                    </td>
+                </tr>
+                </tbody>
 
-            <tfoot>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td class="text-center"><strong>Total {{Number.parseFloat(total).toFixed(2)}}</strong></td>
-                <td></td>
-            </tr>
-            </tfoot>
-        </table>
+                <tfoot>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="text-center"><strong>Total {{Number.parseFloat(total).toFixed(2)}}</strong></td>
+                    <td></td>
+                </tr>
+                </tfoot>
+            </table>
+        </article>
+
+        <article>
+            <h3>Adresse de livraison</h3>
+
+            <address>
+                <div>{{registration.firstname}}  {{registration.name}}</div>
+                <div>{{registration.address1}}</div>
+                <div>{{registration.address2}}</div>
+                <div>{{registration.postal}}</div>
+                <div>{{registration.city}}</div>
+                <div>{{registration.state}}</div>
+                <div>{{registration.country}}</div>
+                <div>{{registration.phone}}</div>
+                <div>{{registration.email}}</div>
+            </address>
+
+        </article>
+
     </section>
 
 </template>
