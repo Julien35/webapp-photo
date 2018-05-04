371baa00e95254074e4baef451224d9796c42dd4
julien35 <julien.berthau@gmail.com>
2018-05-04 15:58:44 +0200

    Backend : init send mail

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\BrainTreeCheckout;
 use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Swift_Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -44,15 +45,67 @@ class CheckoutController extends Controller
      * @Method({"POST"})
      *
      * @param Request $request
-     *
+     * @param Swift_Mailer $mailer
      * @return JsonResponse
      */
-    public function transaction(Request $request)
+    public function transaction(Request $request, \Swift_Mailer $mailer)
     {
         $data = json_decode($request->getContent(), true);
+        $transaction = $this->brainTreeCheckout->createTransaction($data['amount'], $data['nonce']);
+
+        if ($transaction->success) {
+            $this->sendMail('test mail', $mailer);
+        }
 
         return $this->json(
-            $this->brainTreeCheckout->createTransaction($data['amount'], $data['nonce'])
+            $transaction
         );
+    }
+
+
+    /**
+     * @Route("api/mail", name="mail")
+     * @Method({"GET"})
+     * @param Swift_Mailer $mailer
+     * @return JsonResponse
+     */
+    public function testMail(\Swift_Mailer $mailer)
+    {
+        return $this->json(
+            $this->sendMail('test mail', $mailer)
+        );
+    }
+
+    /**
+     * @param $name
+     * @param \Swift_Mailer $mailer
+     * @return int
+     */
+    public function sendMail($name, \Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('mail@gmail.com')
+            ->setTo('mail@gmail.com')
+            ->setBody(
+                $this->renderView(
+                // templates/emails/registration.html.twig
+                    'emails/quotation.html.twig',
+                    array('name' => $name)
+                ),
+                'text/html'
+            );
+
+        $isSent = false;
+        if ($mailer->send($message, $failures)) {
+//            todo: always true on failure
+
+            $isSent = true;
+//            echo ' SUCCESS SENDING!:';
+        } else {
+//            echo ' SENDING ERROR TO :' . print_R($failures);
+        }
+
+
+        return $isSent;
     }
 }
